f04230aa3edc3e6f962f042a4ec206cb85a828d9
IamZenDevX <zendevx.social@gmail.com>
2024-12-13 14:30:56 +0530

    Update propertyController.js

--- controller/propertyController.js
+++ controller/propertyController.js
@@ -1,5 +1,5 @@
+const ErrorHandler = require('../utils/default/errorHandler');
 const property = require('../models/propertyModel');
-
 
 exports.addpropery = async(req, res, next) => {
 const {area, location, price, propertybhk, propertyid, propertyname, propertystatus, propertytype} = req.body;
@@ -15,9 +15,12 @@ propertyid,
 area, 
 });
 res.json(createdproperty);
+}
 
-
-
+exports.deleteproperty = async(req, res, next) => {
+const {id} = req.params;
+const deletedproperty = await property.findByIdAndDelete(id);
+res.json('Data Deleted successfully !');
 }
 
 exports.getpropery = async(req, res, next) => {
@@ -25,6 +28,24 @@ const {id} = req.params;
 let var_property_List;
  var_property_List = await property.findById(id,'propertyname propertybhk price propertystatus propertyid propertytype location area ');
 res.json(var_property_List);
+}
+
+exports.updateproperty = async(req, res, next) => {
+const {id} = req.params;
+let updatedproperty;
+updatedproperty = await property.findByIdAndUpdate(id,
+{
+area: area,
+propertyname: propertyname,
+propertybhk: propertybhk,
+propertyid: propertyid,
+location: location,
+propertytype: propertytype,
+propertystatus: propertystatus,
+price: price
+}
+, {new: true} );
+res.json(updatedproperty);
 
 
 
